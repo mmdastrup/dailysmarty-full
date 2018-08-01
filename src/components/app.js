dc047e39189554996cb52db4a4a8608466292b35
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-08-01 14:03:24 -0600

    setup logo component and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div>
+          <Logo/>
+        </div>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="daily smarty ui image logo big" src="https://raw.githubusercontent.com/bottega-code-school/DailySmartyUI/500449c1c4ed27498aedc066dba422580b61a547/static/assets/ds_circle_logo.png"/>
+            </div>
+        );
+    }
+}
